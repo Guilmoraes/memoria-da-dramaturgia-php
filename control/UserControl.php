d9429f1206a68e227827a3501e90b35040661339
Guilherme <guilherme@esparta.io>
2019-09-24 22:54:19 -0300

    fix

--- control/UserControl.php
+++ control/UserControl.php
@@ -18,8 +18,8 @@ class UserControl{
 	}
 
 	function findAll(){
-		header("Access-Control-Allow-Origin: *");
-		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+		header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+		header('Access-Control-Allow-Credentials: true');
 		$user = new User();
 		return $user->findAll();
 	}
--- view/user/listar.php
+++ view/user/listar.php
@@ -1,7 +1,6 @@
 <?php
-    header("Access-Control-Allow-Origin: https://memoria-da-dramaturgia-php.herokuapp.com");
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     include '../../control/UserControl.php';
     $userControl = new UserControl();
 
