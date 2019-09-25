14b6af7addb887c5dc33c9a5893fd56e5e92ffbf
Guilherme <guilherme@esparta.io>
2019-09-24 22:49:18 -0300

    fix

--- view/user/listar.php
+++ view/user/listar.php
@@ -1,9 +1,9 @@
 <?php
-    include '../../control/UserControl.php';
-    $userControl = new UserControl();
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: https://memoria-da-dramaturgia-php.herokuapp.com");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    include '../../control/UserControl.php';
+    $userControl = new UserControl();
 
     echo json_encode($userControl->findAll());
 ?>
