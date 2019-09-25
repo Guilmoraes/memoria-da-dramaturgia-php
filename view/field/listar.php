7bb25bc85dcb7677ba2047939ce4eb2b55a7625c
Guilherme <guilherme@esparta.io>
2019-09-24 21:54:16 -0300

    Agora foi

--- view/field/listar.php
+++ view/field/listar.php
@@ -1,6 +1,6 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     include '../../control/FieldControl.php';
     $fieldControl = new FieldControl();
 
