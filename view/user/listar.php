5d91563a58e596443539454e94b94f1e6c6caabd
Guilherme <guilherme@esparta.io>
2019-09-24 22:36:56 -0300

    go

--- view/user/listar.php
+++ view/user/listar.php
@@ -1,10 +1,14 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-include '../../control/UserControl.php';
-$userControl = new UserControl();
+    include '../../control/UserControl.php';
+    $userControl = new UserControl();
+    $res = $userControl->findAll();
+    $res->headers->set('Content-Type', 'application/json');
+    $res->headers->set('Access-Control-Allow-Origin', '*');
+    $res->headers->set('Access-Control-Allow-Credentials', 'true');
+    $res->headers->set('Access-Control-Max-Age', '60');
+    $res->headers->set('Access-Control-Allow-Headers', 'AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2');
+    $res->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-echo json_encode($userControl->findAll());
 
-
+    echo $res;
 ?>
