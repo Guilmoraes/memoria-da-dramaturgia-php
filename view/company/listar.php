b946d2f53192022b00ccb4755756f18dfaa6b90e
Guilherme <guilherme@esparta.io>
2019-09-24 23:53:07 -0300

    go

--- view/company/listar.php
+++ view/company/listar.php
@@ -1,7 +1,6 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+    include '../../config/util.php';
+    cors();
     include '../../control/CompanyControl.php';
     $companyControl = new CompanyControl();
 
